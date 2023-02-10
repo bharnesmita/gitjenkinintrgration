93640a319b3a8efa73b20bf7bec952be3cccb373
bharnesmita <66299305+bharnesmita@users.noreply.github.com>
2023-02-10 12:10:56 +0530

    Update sum.java

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public class sum
 {  
 public static void main(String args[])   
 {  
-int n1 = 100, n2 = 200, sum;  
+int n1 = 50, n2 = 30, sum;  
 sum = n1 + n2;  
 System.out.println("The sum of numbers is: "+sum);  
 }  
